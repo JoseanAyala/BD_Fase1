f5208f0c83f3a557dbd8978b634b65e67bb56db4
JoseanAyala <josean.ayala@upr.edu>
2018-03-03 13:17:02 -0400

    Add existing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA, Component } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot([
+     {
+       path:'home', component: HomeComponent
+     },
+     {
+      path:'products', component: ProductsComponent
+     }
+    ])
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
